cf9b6913f62fe6bcbd8b903f2533045d423e12b3
Leila Methnani <lulock@users.noreply.github.com>
2025-02-27 16:15:03 +0100

    Update dashboard_v2.py

--- dashboard_v2.py
+++ dashboard_v2.py
@@ -990,6 +990,5 @@ def style_selected_rows(sel_rows, k):
     return f" ",  {"scatter": plots, "all": fig, "diffs": diffs}
 
 if __name__ == "__main__":
-    app.run_server(debug = "True", port=8050)
-
+    app.run_server(debug = "True", host="0.0.0.0")
 
